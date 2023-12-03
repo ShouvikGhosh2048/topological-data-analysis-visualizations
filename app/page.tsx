1ca2a056a0f140e955616773d029a7bc120a9ef6
Shouvik Ghosh <shouvikg.cal@gmail.com>
2023-12-03 12:56:01 +0530

    Homology explanation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
+    <div className="p-5 space-y-5">
       <h1 className="text-center text-xl p-3">A website for topological data analysis visualizations.</h1>
+      <div className="space-y-2">
+        <h2 className="text-xl">Explanations</h2>
+        <ul className="list-disc pl-5">
+          <li><Link href='/explanation/homology' className="underline">Homology</Link></li>
+        </ul>
+      </div>
     </div>
   );
 }
